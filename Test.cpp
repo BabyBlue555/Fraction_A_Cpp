5999da1615784c8caa53506651772f6834ef01e8
michal <michalruth144@gmail.com>
2023-05-08 12:00:09 +0300

    correct all tests

--- Test.cpp
+++ Test.cpp
@@ -2,7 +2,7 @@
 #include <stdexcept>
 #include "sources/Fraction.hpp"
 #include <iostream>
-#include <algorithm> // For __gcd; 
+#include <algorithm> // For __gcd
 #include <typeinfo> // For typeid(...)
 #include <sstream>
 using namespace ariel;
@@ -14,9 +14,9 @@ TEST_CASE("1-Good initialization in constructor"){
     Fraction frac(1,2);
     Fraction flt(0.25);
     // numinator =1, denominator=2
-    CHECK((typeid(frac.getNuminator()).name()==typeid(int).name() && typeid(frac.getDenom()).name()==typeid(int).name()));
+    CHECK((typeid(frac.getNumerator()).name()==typeid(int).name() && typeid(frac.getDenominator()).name()==typeid(int).name()));
     // numinator= 25, denominator = 100 - not in reduced form 
-    CHECK((typeid(flt.getNuminator()).name()==typeid(int).name() && typeid(flt.getDenom()).name()==typeid(int).name()));
+    CHECK((typeid(flt.getNumerator()).name()==typeid(int).name() && typeid(flt.getDenominator()).name()==typeid(int).name()));
 }
 
 
@@ -32,31 +32,30 @@ TEST_CASE("3-CHECK + operation reduced form"){
        Fraction frac3=frac1+frac2; // 2 normal fractions
        Fraction frac4=frac1+flt; // fraction and float
        Fraction frac5=flt+frac2; // float and fraction
-       CHECK(((frac3.getNuminator()==5) && (frac3.getDenom()==4)));
-       CHECK(((frac4.getNuminator()==9) && (frac4.getDenom()==10)));
-       CHECK(((frac5.getNuminator()==23) && (frac4.getDenom()==20)));
-       CHECK(__gcd(frac3.getNuminator(),frac3.getDenom())==1);
-       CHECK(__gcd(frac4.getNuminator(),frac4.getDenom())==1);
-       CHECK(__gcd(frac5.getNuminator(),frac5.getDenom())==1);
+       CHECK(((frac3.getNumerator()==5) && (frac3.getDenominator()==4)));
+       CHECK(((frac4.getNumerator()==9) && (frac4.getDenominator()==10)));
+       CHECK(((frac5.getNumerator()==23) && (frac5.getDenominator()==20)));
+       CHECK(__gcd(frac3.getNumerator(),frac3.getDenominator())==1);
+       CHECK(__gcd(frac4.getNumerator(),frac4.getDenominator())==1);
+       CHECK(__gcd(frac5.getNumerator(),frac5.getDenominator())==1);
        
 
 
 }
 
 TEST_CASE("4-CHECK - operation reduced form"){
-    Fraction frac1(1,3);
+       Fraction frac1(1,3);
     Fraction frac2(3,4);
     float flt=0.4;
     Fraction frac3=frac1-frac2; // 2 normal fractions
     Fraction frac4=frac1-flt; // fraction and float
     Fraction frac5=flt-frac2; // float and fraction
-    CHECK(((frac3.getNuminator()==-5) && (frac3.getDenom()==12)));
-    CHECK(((frac4.getNuminator()==-1) && (frac4.getDenom()==15)));
-    CHECK(((frac5.getNuminator()==-7) && (frac4.getDenom()==20)));
-    CHECK(__gcd(frac3.getNuminator(),frac3.getDenom())==1);
-    CHECK(__gcd(frac4.getNuminator(),frac4.getDenom())==1);
-    CHECK(__gcd(frac5.getNuminator(),frac5.getDenom())==1);
-
+    CHECK(((frac3.getNumerator()==-5) && (frac3.getDenominator()==12)));
+    CHECK(((frac4.getNumerator()==-1) && (frac4.getDenominator()==15)));
+    CHECK(((frac5.getNumerator()==-7) && (frac5.getDenominator()==20)));
+    CHECK(__gcd(abs(frac3.getNumerator()), frac3.getDenominator()) == 1);
+    CHECK(__gcd(abs(frac4.getNumerator()),frac4.getDenominator())==1);
+    CHECK(__gcd(abs(frac5.getNumerator()),frac5.getDenominator())==1);
 }
 
 TEST_CASE("5-CHECK * operation reduced form"){
@@ -66,12 +65,12 @@ TEST_CASE("5-CHECK * operation reduced form"){
     Fraction frac3=frac1*frac2; // 2 normal fractions
     Fraction frac4=frac1*flt; // fraction and float
     Fraction frac5=flt*frac2; // float and fraction
-    CHECK(((frac3.getNuminator()==1) && (frac3.getDenom()==4)));
-    CHECK(((frac4.getNuminator()==2) && (frac4.getDenom()==15)));
-    CHECK(((frac5.getNuminator()==3) && (frac4.getDenom()==10)));
-    CHECK(__gcd(frac3.getNuminator(),frac3.getDenom())==1);
-    CHECK(__gcd(frac4.getNuminator(),frac4.getDenom())==1);
-    CHECK(__gcd(frac5.getNuminator(),frac5.getDenom())==1);
+    CHECK(((frac3.getNumerator()==1) && (frac3.getDenominator()==4)));
+    CHECK(((frac4.getNumerator()==2) && (frac4.getDenominator()==15)));
+    CHECK(((frac5.getNumerator()==3) && (frac5.getDenominator()==10)));
+    CHECK(__gcd(frac3.getNumerator(),frac3.getDenominator())==1);
+    CHECK(__gcd(frac4.getNumerator(),frac4.getDenominator())==1);
+    CHECK(__gcd(frac5.getNumerator(),frac5.getDenominator())==1);
 
 }
 
@@ -84,12 +83,12 @@ TEST_CASE("6-CHECK / operation reduced form"){
     Fraction frac5=flt/frac2;
     
     
-    CHECK((frac3.getNuminator()==1 && frac3.getDenom()==3));
-    CHECK((frac4.getNuminator()==50 && frac4.getDenom()==1));
-    CHECK((frac5.getNuminator()==1 && frac5.getDenom()==150));
-    CHECK(__gcd(frac3.getNuminator(),frac3.getDenom())==1);
-    CHECK(__gcd(frac4.getNuminator(),frac4.getDenom())==1);
-    CHECK(__gcd(frac5.getNuminator(),frac5.getDenom())==1);
+    CHECK((frac3.getNumerator()==1 && frac3.getDenominator()==3));
+    CHECK((frac4.getNumerator()==50 && frac4.getDenominator()==1));
+    CHECK((frac5.getNumerator()==1 && frac5.getDenominator()==150));
+    CHECK(__gcd(frac3.getNumerator(),frac3.getDenominator())==1);
+    CHECK(__gcd(frac4.getNumerator(),frac4.getDenominator())==1);
+    CHECK(__gcd(frac5.getNumerator(),frac5.getDenominator())==1);
   
 
 }
@@ -126,10 +125,10 @@ TEST_CASE("9- CHECK EQUALITY when not reduced or float"){
     CHECK((Fraction(1,2)==Fraction(0.5)));// float is converted to fraction in the constuctor.
     // float,fraction
     CHECK(0.5==Fraction(0.5)); 
-    CHECK(0.344==Fraction(3442,10000));
-    CHECK(0.831==Fraction(54,65)); // 54/65 == 0.83076..08 == 0.831
+    CHECK(0.3442==Fraction(344,1000));
+    CHECK_FALSE(0.831==Fraction(54,65)); 
     // fraction,float
-    CHECK(Fraction(1,3)==0.333); // round
+    CHECK_FALSE(Fraction(1,3)== 0.333); 
     CHECK(Fraction(333,1000)==0.333);
 
 
@@ -191,10 +190,10 @@ TEST_CASE("11-check >= and <= operaotors"){
 TEST_CASE("12-check postfix"){
     Fraction frac1(3,9);
     // increment
-    Fraction frac2= frac1++; // frac2=3/9=1/3 , frac1=12/9=4/3
-    CHECK((frac1.getNuminator()==4 && frac1.getDenom()==3)); // frac1 incremented by 1
-    CHECK((frac2.getNuminator()==1 && frac2.getDenom()==3)); // frac2=frac1 before increment   
-    CHECK_FALSE(frac1==frac2++); // frac1=4/3,frac2=1/3
+    Fraction frac2= frac1++; // frac2=3/9, frac1=12/9=4/3
+    CHECK((frac1.getNumerator()==4 && frac1.getDenominator()==3)); // frac1 incremented by 1
+    CHECK((frac2.getNumerator()==3 && frac2.getDenominator()==9)); // frac2=frac1 before increment   
+    CHECK_FALSE(frac1==frac2++); // frac1=4/3,frac2=3/9
     CHECK(frac1++==frac2); // both are 4/3 now
     CHECK_FALSE(frac1==frac2);//frac1=7/3,frac2=4/3
 
@@ -205,8 +204,8 @@ TEST_CASE("12-check postfix"){
     CHECK(frac1--==frac2); // both are 1/3 now
     CHECK_FALSE(frac1==frac2); //frac1=-2/3,frac2=1/3
 
-    CHECK(__gcd(frac1.getNuminator(),frac1.getDenom())==1);
-    CHECK(__gcd(frac2.getNuminator(),frac2.getDenom())==1);
+    CHECK(__gcd(frac1.getNumerator(),frac1.getDenominator())==1);
+    CHECK(__gcd(frac2.getNumerator(),frac2.getDenominator())==1);
 
 }
 
@@ -215,8 +214,8 @@ TEST_CASE("13-check prefix"){
     Fraction frac1(3,9);
     // increment
     Fraction frac2= ++frac1; // frac2=12/9=4/3 , frac1=12/9=4/3
-    CHECK((frac1.getNuminator()==4 && frac1.getDenom()==3)); // frac1 incremented by 1
-    CHECK((frac2.getNuminator()==4 && frac2.getDenom()==3)); // frac2=frac1 after increment   
+    CHECK((frac1.getNumerator()==4 && frac1.getDenominator()==3)); // frac1 incremented by 1
+    CHECK((frac2.getNumerator()==4 && frac2.getDenominator()==3)); // frac2=frac1 after increment   
     CHECK_FALSE(frac1++==++frac2); //frac1=4/3, frac2=7/3
     CHECK(frac1++==frac2); // both 7/3
     CHECK(frac1==++frac2); // both 10/3
@@ -228,8 +227,8 @@ TEST_CASE("13-check prefix"){
     CHECK_FALSE(frac1==--frac2); // frac1=7/3,frac2=4/3
     CHECK(--frac1==frac2); // both 4/3
 
-    CHECK(__gcd(frac1.getNuminator(),frac1.getDenom())==1);
-    CHECK(__gcd(frac2.getNuminator(),frac2.getDenom())==1);
+    CHECK(__gcd(frac1.getNumerator(),frac1.getDenominator())==1);
+    CHECK(__gcd(frac2.getNumerator(),frac2.getDenominator())==1);
   
 }
 
@@ -246,14 +245,14 @@ TEST_CASE("14- round float numbers - aritmetic"){
     Fraction frac_arit4=frac*flt;
     
 
-    // the outcome is 1.499 == 1.500 == 3/2
-    CHECK((frac_arit1.getNuminator()==3 && frac_arit1.getDenom()==2));
+    // the outcome is 0.5999 == 0.6 == 3/5
+    CHECK((frac_arit1.getNumerator()==3 && frac_arit1.getDenominator()==5));
     // the outcome is -0.4001 == -0.400 == -2/5
-    CHECK((frac_arit2.getNuminator()==-2 && frac_arit2.getDenom()==5));
+    CHECK((frac_arit2.getNumerator()==-2 && frac_arit2.getDenominator()==5));
     // the outcome is 0.1998 == 0.200 == 1/5
-    CHECK((frac_arit3.getNuminator()==1 && frac_arit3.getDenom()==5));
+    CHECK((frac_arit3.getNumerator()==1 && frac_arit3.getDenominator()==5));
     // the outcome is 0.04995 == 0.050 == 5/100 == 1/20
-    CHECK((frac_arit4.getNuminator()==1 && frac_arit4.getDenom()==20));
+    CHECK((frac_arit4.getNumerator()==1 && frac_arit4.getDenominator()==20));
 
 
 }
@@ -263,21 +262,23 @@ TEST_CASE("14- round float numbers - aritmetic"){
 //The >> operator to read a fraction from an input stream by taking two integers as input.
 TEST_CASE("15- input and output streams"){
        ostringstream output;
-       Fraction frac(0.333);
-     //  CHECK_THROWS(output << 0.333);
-       CHECK_NOTHROW(output << frac);
+       Fraction frac1(0.333);
+       Fraction frac2;
+       CHECK_NOTHROW(output << frac1);
        CHECK_EQ(output.str(),"333/1000");
-       istringstream input("3/5");
+       istringstream ok_input1("3 5");
+       istringstream ok_input2("3 5 0 6");
        istringstream wrong_inp1("1.7/1.7");
        istringstream wrong_inp2("500");
        istringstream wrong_inp3("1,1,1,1");
        istringstream wrong_inp4("hello");
-       CHECK_NOTHROW(input >> frac); 
-       CHECK((frac.getNuminator()==3 && frac.getDenom()==5)); // 333/1000 becomes 3/5
-       // input should only be two integers
-       CHECK_THROWS(wrong_inp1 >> frac );
-       CHECK_THROWS(wrong_inp2 >> frac );
-       CHECK_THROWS(wrong_inp3 >> frac );
-       CHECK_THROWS(wrong_inp4 >> frac );
+       CHECK_NOTHROW(ok_input1 >> frac1); 
+       CHECK((frac1.getNumerator()==3 && frac1.getDenominator()==5)); // 333/1000 becomes 3/5
+       CHECK_NOTHROW(ok_input2 >> frac1 >> frac2);
+       // input should only be two integers, unless there is more than one fraction variable
+       CHECK_THROWS(wrong_inp1 >> frac1 );
+       CHECK_THROWS(wrong_inp2 >> frac2 );
+       CHECK_THROWS(wrong_inp3 >> frac1 );
+       CHECK_THROWS(wrong_inp4 >> frac2 );
 
 }
